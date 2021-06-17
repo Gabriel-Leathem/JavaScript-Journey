1140479f4c1e46349cb4b0582f3d9fb1099c221a
Gabriel Leathem <gabsthedestroyer@gmail.com>
2021-06-17 20:58:47 +1000

    Fully Functional Event Listener driven Canvas Piece
    After doing version 1, creating multiple circles on the screen and bouncing them off the edges of the screen and having velocities etc, HUGE additions and major improvements in second and final version, to things like dynamic circle spawning and canvas resizing. Project 1 complete!

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -48,18 +48,49 @@ let ctx = canvas.getContext('2d');
 // let dy = (Math.random() - 0.5) * 10;
 // let radius = 30;
 
-function Circle(x, y, dx, dy, radius) {
+let mouse = {
+	x: undefined,
+	y: undefined,
+}
+
+let maxRadius = 40;
+// let minRadius = 2;
+
+let colorArray = [
+	'#2F2D38',
+	'#BBF2D8',
+	'#418267',
+	'#E79F86',
+	'#A63737'	
+];
+
+//Event listeners
+window.addEventListener("mousemove", function(event) {
+	mouse.x = event.x;
+	mouse.y = event.y;
+});
+
+window.addEventListener("resize", function() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	init();
+});
+
+class Circle {
+	constructor(x, y, dx, dy, radius) {
 	this.x = x;
 	this.y = y;
 	this.dx = dx;
 	this.dy = dy;
 	this.radius = radius;
+	this.minRadius = radius;
+	this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
 	this.draw = function () {
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		ctx.strokeStyle = "blue";
-		ctx.stroke();
+		ctx.fillStyle = this.color;
 		ctx.fill();
 	}
 
@@ -75,19 +106,33 @@ function Circle(x, y, dx, dy, radius) {
 	this.x += this.dx;
 	this.y += this.dy;
 
+	//interactivity
+	if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+		if (this.radius < maxRadius) {
+			this.radius += 1;
+		}
+	} else if (this.radius > this.minRadius) {
+		this.radius -= 1;
+	}
+
 	this.draw();
+		}
 	}
 }
 
 let circleArray = [];
 
-for (var i = 0; i < 100; i++) {
-	let radius = 30;
-	let x = Math.random() * (innerWidth - radius * 2) + radius;
-	let y = Math.random() * (innerHeight - radius * 2) + radius;
-	let dx = (Math.random() - 0.5) * 10;
-	let dy = (Math.random() - 0.5) * 10;
-	circleArray.push(new Circle(x, y, dx, dy, radius));
+function init() {
+
+	circleArray = [];
+	for (var i = 0; i < 800; i++) {
+		let radius = (Math.random() * 3 + 1);
+		let x = Math.random() * (innerWidth - radius * 2) + radius;
+		let y = Math.random() * (innerHeight - radius * 2) + radius;
+		let dx = (Math.random() - 0.5) * 2;
+		let dy = (Math.random() - 0.5) * 2;
+		circleArray.push(new Circle(x, y, dx, dy, radius));
+	}
 }
 
 function animate() {
@@ -98,7 +143,6 @@ function animate() {
 	for (var i = 0; i < circleArray.length; i++) {
 		circleArray[i].update();
 	}
-	
 }
 
 animate();
